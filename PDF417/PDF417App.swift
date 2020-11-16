651c85c63e1aadacfbad35ea5b6db9f636a064af
Vishweshwaran <vishwa300499@gmail.com>
2020-11-16 19:54:06 +0530

    Initial Commit

---
+++ PDF417/PDF417App.swift
@@ -0,0 +1,17 @@
+//
+//  PDF417App.swift
+//  PDF417
+//
+//  Created by Vishweshwaran on 16/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PDF417App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
